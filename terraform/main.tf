e47bdb35b9f998f7db537117e22f401c90399bc3
RISHABH <rishabh.goyal@lufthansa-group.com>
2025-04-20 23:06:14 +0200

    update name for uniqueness

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "workspace_rg" {
 module "function_app" {
   source              = "../modules/functions"
   resource_group_name = azurerm_resource_group.workspace_rg.name
-  function_app_names  = ["ingest1", "transform2"]  # Pass the list
+  function_app_names  = ["ingest001sales", "transform001sales"]  # Pass the list
 }
 
 module "storage" {
