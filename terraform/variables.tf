5d70bd530b543b2af3884bada0fe77d1d71a5c5f
RISHABH <rishabh.goyal@lufthansa-group.com>
2025-04-20 18:12:44 +0200

    remove vars sp

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
-variable "ARM_SUBSCRIPTION_ID" {
-  type = string
-}
-variable "ARM_TENANT_ID" {
-  type = string
-}
+# variable "ARM_SUBSCRIPTION_ID" {
+#   type = string
+# }
+# variable "ARM_TENANT_ID" {
+#   type = string
+# }
 
-variable "ARM_CLIENT_ID" {
-  type = string
-}
+# variable "ARM_CLIENT_ID" {
+#   type = string
+# }
 
-variable "ARM_CLIENT_SECRET" {
-  type = string
-}
+# variable "ARM_CLIENT_SECRET" {
+#   type = string
+# }
 
 variable "product_id" {
   type = string
