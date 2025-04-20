45e5793a6a48fc6fcb8360f2022bc299aac1e0e4
RISHABH <rishabh.goyal@lufthansa-group.com>
2025-04-20 16:13:09 +0200

    sp auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,11 @@ terraform {
 provider "azurerm" {
   # Configuration options
   subscription_id     = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   storage_use_azuread = true
   features {}
+  
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,17 @@
 variable "subscription_id" {
   type = string
 }
+variable "tenant_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
 
 variable "product_id" {
   type = string
